b7407ae69a40ebc267932f2b03649a611ffedbe7
ephimeteus <109197865+ephimeteus@users.noreply.github.com>
2023-10-01 19:54:15 -0300

    init-branch

---
+++ react-rpg/src/Pages/Store/Purchase/Purchase.jsx
@@ -0,0 +1,5 @@
+const Purchase = () => {
+    return <h2>Purchase</h2>
+};
+
+export default Purchase;
--- react-rpg/src/Pages/Store/Store.jsx
+++ react-rpg/src/Pages/Store/Store.jsx
@@ -1,5 +1,13 @@
+import Purchase from "./Purchase/Purchase";
+
 const Store = () => {
-   return <h1>Store</h1>
+   return (
+        <>
+            <h1>Store</h1>
+            <Purchase />
+        </>
+    );
+
 };
 
 export default Store;
