ef0eae2d459a7f05daec6221e8c7dacdbd699278
ephimeteus <109197865+ephimeteus@users.noreply.github.com>
2023-10-11 21:08:46 -0300

    added items.json and card styling

--- react-rpg/src/Components/Header/Header.jsx
+++ react-rpg/src/Components/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className="header">
             <div className='portrait'></div>
-            <div className='quote'>"Within each of these lies a unique tale, dear to my heart. I shall not relinquish them for a trifling sum."</div>
+            <div className='quote'>"Within each of these lies a unique tale. I shall not relinquish them for a trifling sum."</div>
         </div>
     );
 };
