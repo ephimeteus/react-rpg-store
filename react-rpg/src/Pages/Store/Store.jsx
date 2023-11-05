c50e057534fd60041be2b0e53a6b8139dd7adea8
ephimeteus <109197865+ephimeteus@users.noreply.github.com>
2023-11-05 00:55:12 -0300

    Added individual quotes

--- react-rpg/src/Pages/Store/Store.jsx
+++ react-rpg/src/Pages/Store/Store.jsx
@@ -5,17 +5,22 @@ import CharacterSheet from '../Character/CharacterSheet/CharacterSheet';
 import items from '../Store/_mocks/items.json'
 
 
-
-
 const Store = () => {
    const [selectedCharacter, setSelectedCharacter] = useState(null);
    const onSelectCharacter = (name) => { setSelectedCharacter(name)};
+
+   let quote = 
+   selectedCharacter === 'Al Jabik'
+   ? 'I offer relics whose brilliance has waned, treasures reclaimed from the forsaken realms only I dare to tread.'
+   : selectedCharacter === 'Miriam'
+   ? 'I deal in the shadow, where hope is a fleeting memory'
+   :'In the depths of my trove, madness and salvation are entwined.';
    
    return (
       <>
          <CharacterSheet onSelectCharacter={onSelectCharacter} />
          <div>
-            <Header name= {selectedCharacter} quote='I paid the blood price for these. And my blood is not cheap'/>
+            <Header name= {selectedCharacter} quote={ quote } />
             <CardGrid selectedCharacter={selectedCharacter} items={items} />
          </div>
       </>
